2512164b3024a242eba9d5a09443e9f60f303924
devmonzer08 <142656890+devmonzer08@users.noreply.github.com>
2023-09-24 05:38:48 +0300

    Add files via upload

--- zthon/plugins/رشق تفاعل.py
+++ zthon/plugins/رشق تفاعل.py
@@ -1,7 +1,5 @@
-#ZE
-#الملـف حقـوق وكتابـة مودي الهيبـه ™ @UP_UO 
-#جميع الحقوق تابعه لسورس زد إي » @UI_XB 
-#الملـف كتابـة الهيبه مودي .
+#refz ®
+#الملـف حقـوق وكتابـة المبرمج سينزر
 import asyncio
 import os
 import sys
@@ -13,27 +11,27 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest as unblock
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from HuRe import l313l
+from zthon import zedub
 
 from ..core.managers import edit_or_reply
 
 
 
-@l313l.ar_cmd(pattern="رشق ?(.*)")
+@zedub.zed_cmd(pattern="رشق تفاعل  ?(.*)")
 async def zilzal(event):
     card = event.pattern_match.group(1)
-    chat = "@RSHQ1000bot"
+    chat = "@tf4lbot"
     reply_id_ = await reply_id(event)
-    ar = await edit_or_reply(event, "**جـاري رشـق المنشور انتظـر قليـلًا ... 💡**")
+    zed = await edit_or_reply(event, "**جـاري رشـق المنشور انتظـر قليـلًا ... 💡**")
     async with event.client.conversation(chat) as conv:
         try:
             await conv.send_message(card)
         except YouBlockedUserError:
-            await l313l(unblock("RSHQ1000bot"))
+            await zedub(unblock("@دtf4lbot"))
             await conv.send_message(card)
         await asyncio.sleep(2)
         response = await conv.get_response()
         await event.client.send_read_acknowledge(conv.chat_id)
         await event.client.send_message(event.chat_id, response.message)
-        await ar.delete()
+        await zed.delete()
 
